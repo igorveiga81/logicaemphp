682aed0d87c562e3c98f5ba1d061549d73640b8c
IGOR VEIGA OLIVEIRA <igor.voliveira4@senacsp.edu.br>
2025-11-26 14:55:49 -0300

    atualização da aula 26-11-25

--- aula02-operadores/index.php
+++ aula02-operadores/index.php
@@ -22,8 +22,89 @@
     echo "resultado da multiplicação e $multiplicacao <br>";
     echo "resultado da potenciação e $potenciacao <br>";
     echo "resultado da divisão e $divisao <br>";
-    echo "resultado do rsto da divisão de $a e $b e $restodadivisao <br>"
+    echo "resultado do rsto da divisão de $a e $b e $restodadivisao <br>";
 
+    echo "============================================================";
+    //Operadores de Atribuição
+ 
+    echo "<h1> Operadores de Atribuição </h1>"; //Titulo
+ 
+    $rua = "<br><br> Rua Amador Bueno";
+    $rua .= " na Cidade de São Paulo";
+ 
+    $preco = 2.45;
+    $preco += 2.30;
+    $preco -= 1.30;
+ 
+    // Formata o número para usar vírgula como separador decimal
+    $preco_formatado = number_format($preco, 2, ',', '.' );
+ 
+    // Exisbe o Resultado
+    echo "$rua, <br> Preço R$ $preco_formatado <br> <br>";
+ 
+    // Operadores de Comparação
+ 
+    $c = 10;
+    $d = "10";
+ 
+    echo "<h1> Comparação de igualdade </h1>"; // <h1> </h1> Titulo podendo mudar de tamanho para h2/h3...
+    echo ($c == $d) ? "São iguais." : "Não são iguais."; //? é tipo true e : é false / se senão
+    echo "<br>";
+    echo($c ===$d) ? "São Estritamente iguais." : "Não são estritamente iguais."; //? é tipo true e : é false
+ 
+    echo "<h1> Comparação de Diferença </h1>";
+ 
+    echo ($c != $d) ? "São Diferentes." : "Não são diferentres.";
+    echo "<br>";
+    echo ($c !== $d) ? "São estritamente diferentes." : "Não são estritamente diferentes.";
+ 
+    $teste = false;
+    echo "<h1> Negação </h1>";
+    echo (!$teste) ? "É verdadeiro." : "É falso.";
+ 
+    $e = 5;
+    $f = 3;
+ 
+    echo "<h1> Maior que e Menor que </h1>";
+    echo ($e > $f) ? "E é maior que F." : "E Não é Maior que F";
+    echo "<br>";
+    echo ($e < $f) ? "E é maior que F." : "E não é Maior que F";
+ 
+    $h = 4;
+    $i = 4;
+    $j = 2;
+    echo "<h1> Maior ou Igual a, Menor ou Igual a </h1>";
+ 
+    echo ($h > $i) ? "H é maior ou igual a I" : "H não é maior ou igual a I";
+    echo "<br>";
+    echo ($i <= $j) ? "I é menor ou igual a J" : "I não é menor ou igual a J";
+    echo "<br>";
+ 
+    //Operadores Lógicos
+ 
+    echo "<h1> Operadores Logicos </h1>";
+ 
+    $idade = 26;
+    $tem_cnh = true;
+ 
+    echo ($idade >= 18 and $tem_cnh) ? "Você pode dirigir." : "Você não pode dirigir.";
+    echo "<br>";
+ 
+    $tem_ingresso = false;
+    $tem_convitevip = true;
+ 
+    echo($tem_ingresso or $tem_convitevip) ?"Você pode entrar na festa." : "Você não pode entrar na festa.";
+    echo "<br>";
+   
+    // && para and e || para or
+ 
+    $sol = true;
+    $sem_chuva = false;
+    $feriado = true;
+ 
+    echo($sol && $sem_chuva || $feriado) ? "Você pode sair para passear" : "Melhor ficar em casa";
+ 
+ 
     ?>
 </body>
 </html>
